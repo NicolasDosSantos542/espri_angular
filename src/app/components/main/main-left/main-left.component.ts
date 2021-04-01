c24805890b8d00d6f905b3bfd4d308b8e6c7db73
Nicolas <nicolas.de-meyere-dos-santos@epitech.eu>
2021-04-01 16:48:32 +0200

    import html left-part

--- src/app/components/main/main-left/main-left.component.ts
+++ src/app/components/main/main-left/main-left.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-left.component.css']
 })
 export class MainLeftComponent implements OnInit {
-
+    soulArray = {Essence : 'Prévoyance', Anatheme: 'Versatilité'};
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onClick(value) {
+    console.log(value);
+  }
 }
