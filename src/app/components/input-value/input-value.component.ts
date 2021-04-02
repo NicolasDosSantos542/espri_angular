0237fd8fd0d238ae4d2f8129967d6f5c499597c6
Nicolas <nicolas.de-meyere-dos-santos@epitech.eu>
2021-04-02 11:43:15 +0200

    change input-value pour un input seul

--- src/app/components/input-value/input-value.component.ts
+++ src/app/components/input-value/input-value.component.ts
@@ -1,30 +1,35 @@
 import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 
 @Component({
-  selector: 'app-input-value',
-  templateUrl: './input-value.component.html',
-  styleUrls: ['./input-value.component.css']
+    selector: 'app-input-value',
+    templateUrl: './input-value.component.html',
+    styleUrls: ['./input-value.component.css']
 })
 export class InputValueComponent implements OnInit {
 
-  public iconePlus =  'assets/plus.png';
+    public iconePlus = 'assets/plus.png';
 
-  @Input() value;
-  isInEdition = false;
-  str = '';
-  islist: false;
+    @Input() value;
+    isInEdition = false;
+    str = '';
+    islist: false;
 
-  onClick(){
-    this.isInEdition = true;
-  }
+    onClick() {
+        this.isInEdition = true;
+    }
 
-  validate(){
-    this.isInEdition = false;
-    console.log(this.isInEdition);
-  }
+    validate() {
+        if (this.isInEdition === false) {
+            this.isInEdition = true;
+        } else {
+            this.isInEdition = false;
+        }
+        console.log(this.isInEdition);
+    }
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 }
