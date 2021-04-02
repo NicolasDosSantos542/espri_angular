161ff4be90703673e600e5f8fe7a18a1a6febe1c
Nicolas <nicolas.de-meyere-dos-santos@epitech.eu>
2021-04-02 16:34:19 +0200

    affichage des saison avec prompt OK

--- src/app/components/main/main-right/saisons/saisons.component.ts
+++ src/app/components/main/main-right/saisons/saisons.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-saisons',
-  templateUrl: './saisons.component.html',
-  styleUrls: ['./saisons.component.css']
+    selector: 'app-saisons',
+    templateUrl: './saisons.component.html',
+    styleUrls: ['./saisons.component.css']
 })
 export class SaisonsComponent implements OnInit {
+    seasonArray = [
+        {
+            name: 'hiver',
+            id: 'winter',
+            keywords: 'Coordination, Instinct, Mimer, Modifier, Mouvement, Profiter...',
+            isOnLeft: true,
+            score: 'X'
+        }, {
+            name: 'printemps',
+            id: 'spring',
+            keywords: 'Faux, Eviter, Pacifisme,\n' +
+                '            Perception, Tromper, Vérité...',
+            isOnLeft: false,
+            score: 'X'
+        }, {
+            name: 'automne',
+            id: 'autumn',
+            keywords: 'Acquérir, Construire, Définir, Logique, Réparer, Protection...\n' +
+                '\n',
+            isOnLeft: true,
+            score: 'X'
+        }, {
+            name: 'été',
+            id: 'summer',
+            keywords: 'Emotion, Force, Interaction,\n' +
+                '            Rassembler, Sacrifice, Vitesse...',
+            isOnLeft: false,
+            score: 'X'
+        }
+    ];
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+    constructor() {
+    }
 
+    ngOnInit(): void {
+    }
+
+    onSeasonClick(season) {
+        season.score = prompt('quel est votre score?');
+        console.log(season.score);
+        console.log(this.seasonArray[0]);
+    }
 }
