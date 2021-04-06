063f133b98bfc7db5feae075282609da5aa9be5b
Nicolas <nicolas.de-meyere-dos-santos@epitech.eu>
2021-04-06 10:23:40 +0200

    import component season-describe

---
+++ src/app/components/footer/season-describe/season-describe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-season-describe',
+  templateUrl: './season-describe.component.html',
+  styleUrls: ['./season-describe.component.css']
+})
+export class SeasonDescribeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
