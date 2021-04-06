7771eb50b1497d2ad7a0982ce21612c7999a48bc
Nicolas <nicolas.de-meyere-dos-santos@epitech.eu>
2021-04-06 11:19:37 +0200

    modif html-css pour adaptation variables

--- src/app/components/footer/season-describe/season-describe.component.ts
+++ src/app/components/footer/season-describe/season-describe.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-season-describe',
-  templateUrl: './season-describe.component.html',
-  styleUrls: ['./season-describe.component.css']
+    selector: 'app-season-describe',
+    templateUrl: './season-describe.component.html',
+    styleUrls: ['./season-describe.component.css']
 })
 export class SeasonDescribeComponent implements OnInit {
+    seasondescribe = [
+        {
+            name: 'été',
+            id: 'summer',
+            token: 'Token_Ete_small.png',
+            maxim: 'Par ce présent, deviens mon enfant. Par ce cadeau, deviens mon héraut. Par ce don, sois Gardien de ma Saison Porte en ton âme ma Voix et ma ferveur. Prodigue-les au Monde, rends-lui sa saveur.',
+            keywords: [' Vitesse', ' Lumière', ' Emotion', ' Feu', ' Empathie', ' Interaction ', ' Puissance ', ' Passion ', ' Beau ', ' Sacrifice'],
+            color: '#ffd966',
+            essence: 'Emotion',
+            anatheme: 'Facilité'
 
-  constructor() { }
+        },
+        {
+            name: '',
+            id: '',
+            token: '',
+            maxim: '',
+            keywords: [],
+            color: '',
+            essence: '',
+            anatheme: '',
+        },
+        {
+            name: '',
+            id: '',
+            token: '',
+            maxim: '',
+            keywords: [],
+            color: '',
+            essence: '',
+            anatheme: '',
+        },        {
+            name: '',
+            id: '',
+            token: '',
+            maxim: '',
+            keywords: [],
+            color: '',
+            essence: '',
+            anatheme: '',
+        },        {
+            name: '',
+            id: '',
+            token: '',
+            maxim: '',
+            keywords: [],
+            color: '',
+            essence: '',
+            anatheme: '',
+        },
+    ];
 
-  ngOnInit(): void {
-  }
+    constructor() {
+    }
+
+    ngOnInit()
+        :
+        void {
+    }
 
 }
