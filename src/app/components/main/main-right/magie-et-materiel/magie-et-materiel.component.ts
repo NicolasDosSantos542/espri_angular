cebd2b38373ed79ea762c1ccd161fd57bea33e26
Nicolas <nicolas.de-meyere-dos-santos@epitech.eu>
2021-04-07 16:45:17 +0200

    CSS sur magie-matériel

--- src/app/components/main/main-right/magie-et-materiel/magie-et-materiel.component.ts
+++ src/app/components/main/main-right/magie-et-materiel/magie-et-materiel.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-magie-et-materiel',
-  templateUrl: './magie-et-materiel.component.html',
-  styleUrls: ['./magie-et-materiel.component.css']
+    selector: 'app-magie-et-materiel',
+    templateUrl: './magie-et-materiel.component.html',
+    styleUrls: ['./magie-et-materiel.component.css']
 })
 export class MagieEtMaterielComponent implements OnInit {
+    arrayMagic = ['Talisman', 'Liturgie'];
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
